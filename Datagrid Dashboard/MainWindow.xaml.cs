c64c2413f0710d0f510e31632ee7d06994763f0c
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-12-20 23:59:23 +0500

    Extensions

---
+++ Datagrid Dashboard/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Datagrid_Dashboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

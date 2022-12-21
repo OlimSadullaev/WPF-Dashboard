5c8b6072f3a905c5d3d33802aa42b68772e17d74
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-12-21 14:45:03 +0500

    Extensions Error

--- Datagrid Dashboard/MainWindow.xaml.cs
+++ Datagrid Dashboard/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -15,6 +16,37 @@ namespace Datagrid_Dashboard
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private bool IsMaximized = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ClickCount == 2)
+            {
+                if (true)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
+
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Normal;
+                    IsMaximized = true;
+
+                }
+            }
+
         }
     }
 }

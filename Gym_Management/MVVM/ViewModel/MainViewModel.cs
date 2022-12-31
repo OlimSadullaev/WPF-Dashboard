90f84cf0487ff0aafa4307c8ca3a800fdfd861a4
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-12-31 23:33:08 +0500

    Ending main page

---
+++ Gym_Management/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Gym_Management.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Management.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+
+        public HomeViewModel MyProperty { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
--- Gym_Management/obj/Debug/net6.0-windows/App.g.i.cs
+++ Gym_Management/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace Gym_Management {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Gym_Management;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Gym_Management;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);

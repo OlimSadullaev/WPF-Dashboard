b6a4fee903a7bbc60be9967c3ea6688e427c5d6b
Alimkhan <olimxonnutfullaevich@gmail.com>
2023-01-21 23:53:33 +0500

    ViewModels && Commands

---
+++ Reservation/ViewModels/MakeReservationViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Reservation.ViewModels
+{
+    public class MakeReservationViewModel : ViewModelBase
+    {
+        private string _username;
+
+        public string Myproperty
+        {
+            get
+            {
+                return _username;
+            }
+
+            set
+            {
+                _username = value;
+                OnProPertyChange(nameof(Myproperty));
+            }
+        }
+
+        private int _floorNumber;
+
+        public int FloorNumber
+        {
+            get
+            {
+                return _floorNumber;
+            }
+
+            set
+            {
+                _floorNumber = value;
+                OnProPertyChange(nameof(FloorNumber));
+            }
+        }
+
+        private int _roomNumber;
+
+        public int RoomNumber
+        {
+            get
+            {
+                return _roomNumber;
+            }
+
+            set
+            {
+                _roomNumber = value;
+                OnProPertyChange(nameof(RoomNumber));
+            }
+        }
+
+        private DateTime _startDate;
+
+        public DateTime StartDate
+        {
+            get
+            {
+                return _startDate;
+            }
+
+            set
+            {
+                _startDate = value;
+                OnProPertyChange(nameof(StartDate));
+            }
+        }
+
+        private DateTime _endDate;
+
+        public DateTime EndDate
+        {
+            get
+            {
+                return _endDate;
+            }
+
+            set
+            {
+                _endDate = value;
+                OnProPertyChange(nameof(EndDate));
+            }
+        }
+    }
+}
--- Reservation/obj/Debug/net6.0-windows/App.g.i.cs
+++ Reservation/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace Reservation {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Reservation;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Reservation;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);

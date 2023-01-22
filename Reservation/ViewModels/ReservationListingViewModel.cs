7ad46b407b02c027ff09f1b279595af94ab51956
Alimkhan <olimxonnutfullaevich@gmail.com>
2023-01-22 23:22:58 +0500

    ViewModels Updated

--- Reservation/ViewModels/ReservationListingViewModel.cs
+++ Reservation/ViewModels/ReservationListingViewModel.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Reservation.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Reservation.ViewModels
 {
     public class ReservationListingViewModel : ViewModelBase
     {
+
+        private readonly ObservableCollection<ReservationViewModel> _reservations;
+
+        public IEnumerable<ReservationViewModel> Reservations => _reservations;
+        public ICommand MakeReservationCommand { get;}
+
+        public ReservationListingViewModel()
+        {
+            _reservations = new ObservableCollection<ReservationViewModel>();
+            _reservations.Add(new ReservationViewModel(new Reserva(new RoomID(1, 2), "SingletonSean", DateTime.Now, DateTime.Now)));
+            _reservations.Add(new ReservationViewModel(new Reserva(new RoomID(2, 3), "Joe", DateTime.Now, DateTime.Now)));
+            _reservations.Add(new ReservationViewModel(new Reserva(new RoomID(3, 4), "Julia", DateTime.Now, DateTime.Now)));
+        }
     }
 }
---
+++ Reservation/ViewModels/ReservationViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Reservation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation.ViewModels
+{
+    public class ReservationViewModel : ViewModelBase
+    {
+        private Reserva _reservation;
+
+        public string RoomID => _reservation.RoomID?.ToString();
+        public string UserName => _reservation.UserName;
+        public DateTime StarTime => _reservation.StarTime;
+        public DateTime EndTime => _reservation.EndTime;
+
+        public ReservationViewModel(Reserva reservation)
+        {
+            _reservation = reservation;
+        }
+
+    }
+}
